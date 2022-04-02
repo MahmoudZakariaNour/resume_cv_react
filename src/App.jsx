386e53041713fc7fb08bd6b80a369562bc443e69
KhaledAhmedA <khaled.ahmed.alkharashi@gmail.com>
2022-04-02 07:32:24 -0700

    khaled-content2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { contact } from './contact';
 import { GetObjective } from "./Objective";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <contact></contact>
         <GetObjective></GetObjective>
 
       </header>
