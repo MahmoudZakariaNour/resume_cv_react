0cdfa267f9058a95bf2d0ba61a933fa82241027f
MahmoudZakariaNour <mahmoud.noureldin24@gmail.com>
2022-04-02 16:25:30 +0200

    Obj H1

--- src/Objective.jsx
+++ src/Objective.jsx
@@ -2,6 +2,7 @@ export function GetObjective() {
 
     return (
         <>
+        <h1>Career Objective</h1>
             <p>
                 Seeking to work as an application developer in a progressive organization to further my knowledge, utilize my skills and to put 6 years of learnings into use for the growth of the organization.
 
