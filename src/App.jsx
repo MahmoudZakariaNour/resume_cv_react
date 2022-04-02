0b536eb0ee3cb01872d0c42abc6d037f2d139284
MahmoudZakariaNour <mahmoud.noureldin24@gmail.com>
2022-04-02 22:35:21 +0200

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { GetObjective } from "./Objective";
+import { Contact } from "./contact";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <Contact></Contact>
         <GetObjective></GetObjective>
 
       </header>
--- src/contact.jsx
+++ src/contact.jsx
@@ -1,11 +1,13 @@
-export function contact() {
+export function Contact() {
     return (
-        <ul>
-            <li><span>Full Name: </span> ITI Mansoura Branch ITI Student Name</li>
-            <li><span>Address: </span> Where My Step Going</li>
-            <li><span>Age: </span>20 : 50</li>
-            <li><span>Phone Num:</span> 07775000</li>
-            <li><span>myWebSite</span> <a href="">www.mywebsite.com</a></li>
-        </ul>
+        <>
+            <ul>
+                <li><span>Full Name: </span> ITI Mansoura Branch ITI Student Name</li>
+                <li><span>Address: </span> Where My Step Going</li>
+                <li><span>Age: </span>20 : 50</li>
+                <li><span>Phone Num:</span> 07775000</li>
+                <li><span>myWebSite</span> <a href="">www.mywebsite.com</a></li>
+            </ul>
+        </>
     )
 }
